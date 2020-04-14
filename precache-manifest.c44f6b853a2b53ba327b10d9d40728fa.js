0128bd4120b17a31b97b7fe79b7ff59fe3d939cc
Daniel Lepage <dplepage@gmail.com>
2020-04-14 12:53:07 -0400

    Rebuild with github root.

---
+++ precache-manifest.c44f6b853a2b53ba327b10d9d40728fa.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e524edd70a231dc927e72ad0613b336b",
+    "url": "/puzzle-poc/index.html"
+  },
+  {
+    "revision": "b376d66ae7ac809ae8eb",
+    "url": "/puzzle-poc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fac1d33e4a27bc61edb4",
+    "url": "/puzzle-poc/static/js/13.9ad01ade.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/puzzle-poc/static/js/13.9ad01ade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b376d66ae7ac809ae8eb",
+    "url": "/puzzle-poc/static/js/main.27fd6c4e.chunk.js"
+  },
+  {
+    "revision": "0f94bc38a733c9c0c8e5",
+    "url": "/puzzle-poc/static/js/puzz1_1.5b247966.chunk.js"
+  },
+  {
+    "revision": "1dea5ab207a30203aee3",
+    "url": "/puzzle-poc/static/js/puzz1_2-grid-png.6f29d6ab.chunk.js"
+  },
+  {
+    "revision": "14253e9721ea6e91d0af",
+    "url": "/puzzle-poc/static/js/puzz1_2.29ed70d4.chunk.js"
+  },
+  {
+    "revision": "f21ac5b3a116bfd7fc8e",
+    "url": "/puzzle-poc/static/js/puzz2_1.bb5db337.chunk.js"
+  },
+  {
+    "revision": "a4e32c64927d6cafd7ec",
+    "url": "/puzzle-poc/static/js/puzz2_2.a04a7a4d.chunk.js"
+  },
+  {
+    "revision": "f4c864f25dbe4ca03bff",
+    "url": "/puzzle-poc/static/js/puzz3_1.029efa1a.chunk.js"
+  },
+  {
+    "revision": "b3b1aeff618857aee64f",
+    "url": "/puzzle-poc/static/js/puzz3_2.0c33fb11.chunk.js"
+  },
+  {
+    "revision": "1ee227cd363a26322c8d",
+    "url": "/puzzle-poc/static/js/round1.ce908a33.chunk.js"
+  },
+  {
+    "revision": "2e1d170081cb869bf689",
+    "url": "/puzzle-poc/static/js/round2.08481aa8.chunk.js"
+  },
+  {
+    "revision": "e1eff32fb3a3218073b8",
+    "url": "/puzzle-poc/static/js/round3.64f95170.chunk.js"
+  },
+  {
+    "revision": "4aa14f33c68595eb9c11",
+    "url": "/puzzle-poc/static/js/runtime-main.1e14f791.js"
+  },
+  {
+    "revision": "0fe504eaf1f8c9d5998c",
+    "url": "/puzzle-poc/static/js/start.3e6a89a5.chunk.js"
+  },
+  {
+    "revision": "90163b07a7f7e932c8abbc22eb0b200e",
+    "url": "/puzzle-poc/static/media/puzz1_2.grid.90163b07.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6357ae324036dd012b04283a9d39e1a.js"
+  "/puzzle-poc/precache-manifest.c44f6b853a2b53ba327b10d9d40728fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/puzzle-poc/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
